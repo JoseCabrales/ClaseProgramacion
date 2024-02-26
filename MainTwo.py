cb057931742ae3c6bee5cdae6eb112ccf159e7dc
Manuel18morillo <morillomanuel502@gmail.com>
2024-02-26 08:46:00 -0500

    Segundo Commit

---
+++ MainTwo.py
@@ -0,0 +1,6 @@
+from ProgramacionJoseCabrales.ClaseEstudiante import estudiante
+
+
+e = estudiante("Jose" , "cabrales", "jcabrales881@gmail.com","1043296835", "3058228496")
+
+e.Obtener_Informacion()
