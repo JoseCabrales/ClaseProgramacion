6a695145826182d78f6e3b7c1f000e6f3d67082c
JoseCabrales <161320262+JoseCabrales@users.noreply.github.com>
2024-03-08 07:30:44 -0500

    Add files via upload

---
+++ Personas/src/main/java/com/mycompany/personas/Personas.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.personas;
+
+
+public class Personas {
+    
+
+    public static void main(String[] args) {
+        
+       PersonaClase p = new PersonaClase();
+       p.setNombre("Jose Cabrales");
+       
+        System.out.println(p.getNombre());
+        
+    }
+}
